a55b6cbaeba7b76fcc2db79878d93d032881174e
AdwaitGaikwad <adigaikwad9716@gmail.com>
2022-03-18 22:08:44 +0530

    Text update on the login feature

--- SwiftUIDemo/SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Need to implement the login feature over here")
             .padding()
     }
 }
