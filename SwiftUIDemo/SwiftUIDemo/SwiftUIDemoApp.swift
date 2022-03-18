3676909480a1e18d4f1e7843b47d27f78f51e657
AdwaitGaikwad <adigaikwad9716@gmail.com>
2022-03-18 22:06:11 +0530

    Initial project setup
    This commit haves the initial setup push

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Adwait Gaikwad on 18/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
